2a4732174c9b9adecc93abe8642e755610890385
MohabMohamed <mohab.m.mohamed@gmail.com>
2025-04-24 01:09:38 +0200

    chore: change the style to usesemistandard

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const app = require('./app')
+const app = require('./app');
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
